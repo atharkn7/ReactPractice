4023092770ce1aa3d7bb8410b280e0f8950d2281
atharkn7 <athar.kn7@gmail.com>
2026-02-27 17:13:10 +0530

    logo uploaded

--- TravelJournal/src/App.jsx
+++ TravelJournal/src/App.jsx
@@ -1,9 +1,13 @@
 import reactLogo from './assets/react.svg'
+import globe from './assets/globe.png'
+import marker from './assets/marker.png'
 import './App.css'
 
 export default function App() {
   return (
     <>
+      <img src={globe} alt="Globe Logo" />
+      <img src={marker} alt="Marker Logo" />
       <h1>Hello</h1>
     </>
   )
