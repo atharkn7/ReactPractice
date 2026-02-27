addeb85362bf1d9965b816792a2284e798ad332b
atharkn7 <athar.kn7@gmail.com>
2026-02-27 22:59:00 +0530

    Articles struct added

---
+++ TravelJournal/src/components/Entry.jsx
@@ -0,0 +1,19 @@
+export default function Entry() {
+    return (
+        <main>
+            <article>
+                <img src="" alt="Place Image" />
+                <div>
+                    <div>
+                        <img src="" alt="Marker Logo" />
+                        <span>JAPAN</span>
+                        <a href="https://www.google.com/">View of Google Maps</a>
+                    </div>
+                    <h1>Mount Fuji</h1>
+                    <p>Date</p>
+                    <p>Place Text</p>
+                </div>
+            </article>
+        </main>
+    )
+}
