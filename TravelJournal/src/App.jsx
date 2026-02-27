0a4e5c4e8d304a5b401bed1c7024e9e6a16ef601
atharkn7 <athar.kn7@gmail.com>
2026-02-27 22:49:12 +0530

    Header built

--- TravelJournal/src/App.jsx
+++ TravelJournal/src/App.jsx
@@ -1,10 +1,10 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <>
-
+      <Header />
     </>
   )
 }
