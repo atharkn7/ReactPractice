3e35b8e237674c775488be8fd3d01f06f9e21482
atharkn7 <athar.kn7@gmail.com>
2026-02-28 01:12:12 +0530

    Data added from data.js

--- TravelJournal/src/App.jsx
+++ TravelJournal/src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
 import Header from './components/Header'
 import Entry from './components/Entry'
+import data from './data'
 
 export default function App() {
+  const entryElements = data.map((entry) => {
+    return (
+      <Entry
+        key={entry.id}
+        img={entry.img.src}
+        alt={entry.img.alt}
+        title={entry.title}
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        dates={entry.dates}
+        text={entry.text}
+      />
+    )
+  })
+
   return (
     <>
       <Header />
-      <Entry />
+      {entryElements}
     </>
   )
 }
--- TravelJournal/src/components/Entry.jsx
+++ TravelJournal/src/components/Entry.jsx
@@ -1,21 +1,21 @@
 import marker from '../assets/marker.png'
 
-export default function Entry() {
+export default function Entry(props) {
     return (
         <main>
             <article>
                 <div className="main-image-container">
-                    <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Place Image" />
+                    <img src={props.img} alt={props.alt} />
                 </div>
                 <div className='entry'>
                     <div className='entry-location'>
                         <img src={marker} alt="Marker Logo" />
-                        <span>JAPAN</span>
-                        <a href="https://www.google.com/">View of Google Maps</a>
+                        <span>{props.country}</span>
+                        <a href={props.googleMapsLink}>View of Google Maps</a>
                     </div>
-                    <h1>Mount Fuji</h1>
-                    <p className='date'>12 Jan, 2021 - 24 Jan, 2021</p>
-                    <p className='text'>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                    <h1>{props.title}</h1>
+                    <p className='date'>{props.dates}</p>
+                    <p className='text'>{props.text}</p>
                 </div>
             </article>
 
